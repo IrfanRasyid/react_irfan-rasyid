b2bc2646632bffd80fbc82cc12e45f3680b24e40
IrfanRasyid <41520010081@student.mercubuana.ac.id>
2023-10-07 14:16:38 +0700

    Update Summary

---
+++ 19_Code Competence KM React 2/Praktikum/src/components/header/constants.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+import { Button } from "antd";
+
+export const MENU_ITEM = [
+  {
+    key: "1",
+    label: <Link to="/create-product">Create Product</Link>,
+    style: {
+      color: "white",
+      backgroundColor: "#0D6EFD",
+      borderRadius: "5px",
+    },
+  },
+  {
+    key: "2",
+    label: "Pricing",
+  },
+  {
+    key: "3",
+    label: "FAQs",
+  },
+  {
+    key: "4",
+    label: "About",
+  },
+  {
+    key: "5",
+    label: (
+      <Link to="/">
+        <Button
+          size="large"
+          type="primary"
+          onClick={() => {
+            localStorage.removeItem("token");
+          }}
+          danger
+          style={{ color: "white", fontWeight: 600, borderRadius: "5px" }}
+        >
+          Logout
+        </Button>
+      </Link>
+    ),
+  },
+];
--- 21_Deployment/README.md
+++ 21_Deployment/README.md
@@ -1,2 +1,35 @@
 # Deployment
 
+## Vercel
+
+Vercel adalah platform yang dapat digunakan untuk melakukan deployment aplikasi React. Vercel dapat digunakan untuk melakukan deployment secara gratis. Vercel dapat digunakan untuk melakukan deployment aplikasi React dengan mudah.
+
+### Instalasi
+
+Untuk menggunakan Vercel, kita perlu menginstal Vercel CLI. Kita dapat menginstalnya dengan perintah berikut:
+
+```bash
+yarn global add vercel
+```
+
+### Login
+
+Setelah menginstal Vercel CLI, kita perlu login ke Vercel. Kita dapat login dengan perintah berikut:
+
+```bash
+vercel login
+```
+
+### Deployment
+
+Setelah login, kita dapat melakukan deployment aplikasi React dengan perintah berikut:
+
+```bash
+vercel
+```
+
+Kita dapat menambahkan opsi --prod untuk melakukan deployment ke production.
+
+```bash
+vercel --prod
+```
